30411b054e6cd826ce56e6c55098a92d999bf108
cheaven <cheaven888@gmail.com>
2026-04-13 06:58:25 +0000

    product

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,20 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/admin/stats', [AdminController::class, 'stats']);
     });
 });
+
+
+// Public product routes (anyone can view)
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/featured', [ProductController::class, 'getFeatured']);
+Route::get('/products/categories', [ProductController::class, 'getCategories']);
+
+// Hotel product management routes (only hotel users)
+Route::middleware(['auth:sanctum', 'user.type:hotel'])->prefix('hotel')->group(function () {
+    Route::get('/products', [ProductController::class, 'myProducts']);  // View my products
+    Route::post('/products', [ProductController::class, 'store']);      // Create product
+    Route::put('/products/{id}', [ProductController::class, 'update']); // Update product
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']); // Delete product
+    Route::patch('/products/{id}/toggle-availability', [ProductController::class, 'toggleAvailability']); // Toggle available
+    Route::patch('/products/{id}/toggle-featured', [ProductController::class, 'toggleFeatured']); // Toggle featured
+});
